79ae8307bb38794376edef5d5cf93f3c8fd35642
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-19 07:12:34 +0000

    test case passed

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
+ 
 name1_lwr = name1.lower()
 name2_lwr = name2.lower()
+combinedStr = name1_lwr + name2_lwr
 
-tcount = name1_lwr.count('t') + name2_lwr.count('t')
-rcount = name1_lwr.count('r') + name2_lwr.count('r')
-ucount = name1_lwr.count('u') + name2_lwr.count('u')
-ecount = name1_lwr.count('e') + name2_lwr.count('e')
+tcount = combinedStr.count('t')
+rcount = combinedStr.count('r') 
+ucount = combinedStr.count('u') 
+ecount = combinedStr.count('e') 
 trueScore = tcount + rcount + ucount + ecount
 
-lcount = name1_lwr.count('l') + name2_lwr.count('l')
-ocount = name1_lwr.count('o') + name2_lwr.count('o')
-vcount = name1_lwr.count('v') + name2_lwr.count('v')
-ecount = name1_lwr.count('e') + name2_lwr.count('e')
+lcount = combinedStr.count('l') 
+ocount = combinedStr.count('o') 
+vcount = combinedStr.count('v') 
+ecount = combinedStr.count('e') 
 loveScore = lcount + ocount + vcount + ecount
 
 finalScore =  int(str(trueScore) + str(loveScore))
